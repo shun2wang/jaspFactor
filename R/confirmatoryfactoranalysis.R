cbfe0f37c51ba3e03d1c01abb72886fad2927926
Shun Wang <shuonwang@gmail.com>
2023-01-10 11:30:33 +0800

    trigger unit-test

--- R/confirmatoryfactoranalysis.R
+++ R/confirmatoryfactoranalysis.R
@@ -13,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 # This is a temporary fix
 # TODO: remove it when R will solve this problem!
